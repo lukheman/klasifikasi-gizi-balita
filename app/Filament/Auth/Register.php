b5bc867dc28c71216288329effb1dbe4b17fec05
Akmal <akmalthemalong@gmail.com>
2025-04-01 21:48:33 +0800

    feat: tambahkan field dan logika pendaftaran untuk OrangTua di Register
    - Menambahkan field 'tanggal_lahir' dan 'telepon' pada form pendaftaran
    - Mengubah field 'nik' untuk menambahkan label
    - Menambahkan logika pembuatan entitas OrangTua saat pendaftaran

--- app/Filament/Auth/Register.php
+++ app/Filament/Auth/Register.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Auth;
 
+use App\Models\OrangTua;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Hidden;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Pages\Auth\Register as FilamentRegister;
+
+use Illuminate\Database\Eloquent\Model;
 
 class Register extends FilamentRegister { 
 
@@ -16,11 +20,42 @@ class Register extends FilamentRegister {
                         $this->getPasswordFormComponent(),
                         $this->getPasswordConfirmationFormComponent(),
 
-                        TextInput::make('nik'),
-                        Hidden::make('role')
-                            ->default('orangtua')
+                        TextInput::make('nik')
+                            ->label('NIK'),
+                        DatePicker::make('tanggal_lahir')
+                            ->label('Tanggal Lahir')
+                            ->maxDate(now()),
+                        TextInput::make('telepon')
+                            ->label('Telepon')
+                            ->numeric()
                     ])
                     ->statePath('data');
+    }
+
+    public function handleRegistration(array $data): Model {
+
+        $data_user = [
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => $data['password'],
+            'role' => 'orangtua',
+        ];
+
+
+        $user = $this->getUserModel()::create($data_user);
+
+        $data_orang_tua = [
+            'nik' => $data['nik'],
+            'tanggal_lahir' => $data['tanggal_lahir'],
+            'telepon' => $data['telepon'],
+            'id_user' => $user->id
+        ];
+
+        // return $this->getUserModel()::create($data_user);
+
+        OrangTua::create($data_orang_tua);
+
+        return $user;
     }
 
 }
