587e979b2355bede7a8722d8eedd3e8ac5976f51
Akmal <akmalthemalong@gmail.com>
2025-04-01 18:28:21 +0800

    feat(routes): tambah rute baru untuk laporan gizi balita
    - Menambahkan rute baru untuk laporan gizi balita di LaporanController
    - Mengatur rute home untuk menampilkan form login

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\LaporanController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('home');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/laporan-gizi/{id_balita}', [LaporanController::class, 'giziBalita'])->name('laporan.gizi-balita');
