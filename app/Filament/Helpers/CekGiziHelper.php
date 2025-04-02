f06a6aab5009108f4ac4d5dcbe6efd3cb3b70f11
Akmal <akmalthemalong@gmail.com>
2025-04-02 10:44:58 +0800

    style: menghapus spasi berlebih dan baris kosong di CekGiziHelper
    - Menghapus spasi berlebih di akhir baris
    - Menghapus baris kosong yang tidak diperlukan

--- app/Filament/Helpers/CekGiziHelper.php
+++ app/Filament/Helpers/CekGiziHelper.php
@@ -4,17 +4,17 @@ namespace App\Filament\Helpers;
 
 use App\Models\DataLatih;
 
-class CekGiziHelper { 
+class CekGiziHelper {
 
     private static $K = 3;
 
-    public static function cekStatusGizi($umur, $berat, $tinggi) { 
+    public static function cekStatusGizi($umur, $berat, $tinggi) {
 
         $dataLatih = DataLatih::all();
 
         $jarakEuchlidean = [];
 
-        foreach ( $dataLatih as $latih) { 
+        foreach ( $dataLatih as $latih) {
 
             $jarak = self::hitungJarak($umur, $berat, $tinggi, $latih->umur, $latih->berat, $latih->tinggi);
 
@@ -22,7 +22,7 @@ class CekGiziHelper {
 
         }
 
-        usort($jarakEuchlidean, function ($a, $b) { 
+        usort($jarakEuchlidean, function ($a, $b) {
             return $a[0] <=> $b[0];
         });
 
@@ -34,13 +34,13 @@ class CekGiziHelper {
 
     }
 
-    private static function statusTerbanyak(array $data) { 
+    private static function statusTerbanyak(array $data) {
         $counts = [];
 
-        foreach($data as $item) { 
+        foreach($data as $item) {
             $status = $item[1];
 
-            if(!isset($counts[$status])) { 
+            if(!isset($counts[$status])) {
                 $counts[$status] = 0;
             }
 
@@ -55,7 +55,7 @@ class CekGiziHelper {
 
     // fungsi untuk menghitung jarak antara data uji dengan data latih
     // fungsi ini menghitung data uji dengan 1 data latih
-    private static function hitungJarak($ujiUmur, $ujiBerat, $ujiTinggi, $latihUmur, $latihBerat, $latihTinggi) { 
+    private static function hitungJarak($ujiUmur, $ujiBerat, $ujiTinggi, $latihUmur, $latihBerat, $latihTinggi) {
 
         // dump($latihUmur, $latihBerat, $latihTinggi);
 
@@ -64,8 +64,6 @@ class CekGiziHelper {
         $tinggiKuadrat = ($latihTinggi - $ujiTinggi)**2;
 
         // dd($umurKuadrat, $beratKuadrat, $tinggiKuadrat);
-
-
         $jarak = sqrt($umurKuadrat + $beratKuadrat + $tinggiKuadrat);
 
         return $jarak;
