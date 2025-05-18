e1281f37d878296e9ae245ba1362fe82ae1c9d6d
Akmal <akmalthemalong@gmail.com>
2025-05-18 16:09:49 +0800

    feat(DataLatihResource): tambahkan properti navigationSort untuk mengatur urutan navigasi
    Menambahkan properti `navigationSort` dengan nilai default 3 pada `DataLatihResource` untuk menentukan urutan navigasi dalam grup "Data".

--- app/Filament/Ahligizi/Resources/DataLatihResource.php
+++ app/Filament/Ahligizi/Resources/DataLatihResource.php
@@ -26,6 +26,8 @@ class DataLatihResource extends Resource
 
     protected static ?string $navigationGroup = 'Data';
 
+    protected static ?int $navigationSort = 3;
+
     public static function form(Form $form): Form
     {
         return $form
