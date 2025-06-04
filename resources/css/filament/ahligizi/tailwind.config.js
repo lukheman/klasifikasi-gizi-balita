232eca51fa4e6fe911e195b749c1c1c9a4d43e44
Akmal <akmalthemalong@gmail.com>
2025-06-04 19:09:46 +0800

    feat: tambah konfigurasi Tailwind CSS dan custom stylesheet untuk Filament

---
+++ resources/css/filament/ahligizi/tailwind.config.js
@@ -0,0 +1,10 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/Ahligizi/**/*.php',
+        './resources/views/filament/ahligizi/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Instrument Sans', 'ui-sans-serif', 'system-ui', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
+    ],
+};
