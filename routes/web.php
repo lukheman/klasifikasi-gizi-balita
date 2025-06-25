ffb21f99a6378994b0bbe9462ecaf93f1a2afa69
Akmal <akmalthemalong@gmail.com>
2025-06-25 09:22:43 +0800

    feat(routes): tambah route login untuk ahligizi, kader, orangtua, dan pimpinan

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,8 @@ Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth')->
 Route::get('/laporan-gizi/{id_balita}', [LaporanController::class, 'giziBalita'])->name('laporan.gizi-balita');
 Route::get('/laporan-data-balita', [LaporanController::class, 'dataBalita'])->name('laporan.data-balita');
 Route::get('/laporan-riwayat-pemeriksaan', [LaporanController::class, 'riwayatPemeriksaan'])->name('laporan.riwayat-pemeriksaan');
+
+Route::get('/ahligizi/login', fn() => redirect()->route('login'))->name('filament.ahligizi.auth.login');
+Route::get('/kader/login', fn() => redirect()->route('login'))->name('filament.kader.auth.login');
+Route::get('/orangtua/login', fn() => redirect()->route('login'))->name('filament.orangtua.auth.login');
+Route::get('/pimpinan/login', fn() => redirect()->route('login'))->name('filament.pimpinan.auth.login');
